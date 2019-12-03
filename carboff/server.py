75e7e551a9340d04337361a93c4c9ee8b05c45e3
Austin Atchley <austinatchley@gmail.com>
2019-12-03 04:46:08 +0100

    Implement POST handling for forms and json

--- carboff/server.py
+++ carboff/server.py
@@ -1,11 +1,29 @@
 from argparse import ArgumentParser
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
     return {"hello": "world"}
+
+@app.route("/post/datacenter/json", methods=['POST'])
+def data_center_json():
+    data = request.get_json()['data']
+    return data_center(data)
+
+@app.route("/post/datacenter", methods=['POST'])
+def data_center_form():
+    data = request.form.get('data', type=int)
+    return data_center(data)
+
+# Input: number of bytes processed by a data center
+# Return electricity consumption in KwH
+def data_center(data):
+    if data is None or data < 0:
+        return "Error"
+
+    return {"result": 7.2e-11 * data}
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Carboff Server")
