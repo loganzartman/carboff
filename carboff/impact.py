32376558499d3a992d12790d357ecc2f4999a177
Logan Zartman <logan.zartman@utexas.edu>
2019-12-03 19:56:09 -0600

    fix typo and move constants to json blob

--- carboff/impact.py
+++ carboff/impact.py
@@ -1,42 +1,46 @@
-default_carbon_intensity_factor = 519
-kWh_per_byte_data_center = 7.20e-11
-
-kWh_per_byte_network_wired = 4.29e-10
-kWh_per_byte_network_wireless = 1.52e-10
-kWh_per_byte_network_mobile = 8.84e-10
-
-kWh_per_minute_laptop = 3.2e-04
-kWh_per_minute_mobile = 1.1e-04
+estimates = {
+    "carbon_intensity_factor": {
+        "default": 519
+    },
+    "kWh_per_byte": {
+        "data_center": 7.20e-11,
+        "network": {
+            "wired": 4.29e-10,
+            "wireless": 1.52e-10,
+            "mobile": 8.84e-10
+        }
+    },
+    "kWh_per_minute": {
+        "laptop": 3.2e-04,
+        "mobile": 1.1e-04
+    }
+}
 
 def carbon_intensity_factor(location):
-    return default_carbon_intensity_factor
+    return estimates["carbon_intensity_factor"]["default"]
 
 def kWh_per_byte_network(network_type):
-    if network_type == "wireless":
-        return kWh_per_byte_network_wireless
-    elif network_type == "wired":
-        return kWh_per_byte_network_wired
-    elif network_type == "mobile":
-        return kWh_per_byte_network_mobile
+    data = estimates["kWh_per_byte"]["network"]
+    if network_type in data:
+        return data[network_type]
 
     # By default, return wireless
-    return kWh_per_byte_wireless
+    return data["wireless"]
 
 def kWh_per_minute(device_type):
-    if network_type == "laptop":
-        return kWh_per_minute_laptop
-    elif network_type == "mobile":
-        return kWh_per_minute_mobile
+    data = estimates["kWh_per_minute"]
+    if device_type in data:
+        return data[device_type]
 
     # By default, return laptop
-    return kWh_per_minute_laptop
+    return data["laptop"]
 
 def action(*, duration=0, data=0, location=None, device_type=None, network_type=None):
-    kWh_data_center = data * kWh_per_byte_data_center
-    gCO2_data_center = kWh_data_center * default_carbon_intensity_factor
+    kWh_data_center = data * estimates["kWh_per_byte"]["data_center"]
+    gCO2_data_center = kWh_data_center * carbon_intensity_factor(None)
 
     kWh_network = data * kWh_per_byte_network(network_type)
-    gCO2_network = kWh_network * default_carbon_intensity_factor
+    gCO2_network = kWh_network * carbon_intensity_factor(None)
 
     kWh_device = duration * kWh_per_minute(device_type)
     gCO2_device = kWh_device * carbon_intensity_factor(location)
