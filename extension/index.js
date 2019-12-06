eb35b802c86ac72852d1930f790bc22541170baf
Austin Atchley <austinatchley@gmail.com>
2019-12-06 01:59:38 +0100

    fixed the extension

--- extension/index.js
+++ extension/index.js
@@ -1,10 +1,9 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.");
-        alert("Installed");
     });
 });
 
 chrome.webRequest.onCompleted.addListener(function(details) {
     console.log(details);
-});
+},
+{urls: ["<all_urls>"]});
